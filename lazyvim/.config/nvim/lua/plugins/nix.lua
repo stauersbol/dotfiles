db32813b3249007250ef299c9af162a6edd9716c
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2023-07-30 16:54:36 +0200

    feat(lazyvim): add nix language support

---
+++ lazyvim/.config/nvim/lua/plugins/nix.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "nix" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "rnix-lsp" })
+      end
+    end,
+  },
+}
