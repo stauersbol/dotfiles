ff3407fa797d5a467aad78144f7762ce34b05085
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2023-09-09 20:41:11 +0200

    feat(lazyvim): add docker and docker-compose lsp

---
+++ lazyvim/.config/nvim/lua/plugins/docker.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dockerls = {},
+        docker_compose_language_service = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "dockerfile", "yaml" })
+      end
+    end,
+  },
+}
