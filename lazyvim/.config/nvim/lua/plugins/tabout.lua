ba7fef0cd958c3df7c74299d47c201a2b1ff96ba
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2023-11-14 15:45:19 +0100

    fix(lazyvim): tabout load on verylazy

--- lazyvim/.config/nvim/lua/plugins/tabout.lua
+++ lazyvim/.config/nvim/lua/plugins/tabout.lua
@@ -1,7 +1,7 @@
 return {
   {
     "abecodes/tabout.nvim",
-    event = "InsertEnter",
+    event = "VeryLazy",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "hrsh7th/nvim-cmp",
