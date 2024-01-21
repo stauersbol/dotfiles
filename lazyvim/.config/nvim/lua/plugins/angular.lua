1a9d3d8119dc40f52cff10541b107c2481ae4cd2
Lukas Stauersbøl <9026132+stauersbol@users.noreply.github.com>
2024-01-21 19:42:30 +0100

    fix(lazyvim): move from deprecated treesitter

--- lazyvim/.config/nvim/lua/plugins/angular.lua
+++ lazyvim/.config/nvim/lua/plugins/angular.lua
@@ -8,7 +8,7 @@ return {
     },
   },
   {
-    "nvim-treesitter/nvim-treesitter-angular",
+    "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "angular" })
