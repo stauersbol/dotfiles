c0d761bb3240eff35ccb72a0bdf0f08b12a27184
Lukas Stauersboel <9026132+stauersbol@users.noreply.github.com>
2024-04-22 12:46:56 +0200

    fix(lazyvim): remove all language plugins and move into 1 file

---
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        angularls = {},
+        bashls = {},
+        cssls = {},
+        stylelint_lsp = {},
+        dockerls = {},
+        docker_compose_language_service = {},
+        gleam = {},
+        nixd = {},
+        prismals = {},
+        svelte = {},
+        templ = {},
+        unocss = {},
+        v_analyzer = {},
+        volar = {},
+        zls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "dockerfile",
+          "yaml",
+          "angular",
+          "astro",
+          "bash",
+          "gleam",
+          "javascript",
+          "jsdoc",
+          "nix",
+          "prisma",
+          "ssh_config",
+          "svelte",
+          "templ",
+          "v",
+          "vue",
+          "zls",
+        })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "stylelint", "rnix-lsp" })
+      end
+    end,
+  },
+}
