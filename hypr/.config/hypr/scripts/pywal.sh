41a02e887d410331cd12e83918d930af42b01f91
Lukas Stauersboel <9026132+stauersbol@users.noreply.github.com>
2024-08-10 22:43:58 +0200

    feat(hypr): add scripts to help with background and pywall

---
+++ hypr/.config/hypr/scripts/pywal.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+wallpaper_path=$(readlink "$HOME/.current_wallpaper")
+
+check_file() {
+
+	if [ ! -f "$1" ]; then
+		echo "File $1 not found!"
+		exit 1
+	fi
+}
+
+check_file "$wallpaper_path"
+
+# Do not change terminal colors
+sleep 0.2
+wal -i "$HOME/.current_wallpaper" -t -s
