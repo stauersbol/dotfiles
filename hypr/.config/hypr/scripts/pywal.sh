31fbc800773bd3fd827085d951ce24df2b1ff1ff
Slashiy <9026132+stauersbol@users.noreply.github.com>
2024-12-20 21:46:30 +0100

    style(pywal): style pywal script

--- hypr/.config/hypr/scripts/pywal.sh
+++ hypr/.config/hypr/scripts/pywal.sh
@@ -4,10 +4,10 @@ wallpaper_path=$(readlink "$HOME/.current_wallpaper")
 
 check_file() {
 
-	if [ ! -f "$1" ]; then
-		echo "File $1 not found!"
-		exit 1
-	fi
+  if [ ! -f "$1" ]; then
+    echo "File $1 not found!"
+    exit 1
+  fi
 }
 
 check_file "$wallpaper_path"
