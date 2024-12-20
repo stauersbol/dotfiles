2f8aa5431178d0f383f7a2d94f086f30f6306d2d
Slashiy <9026132+stauersbol@users.noreply.github.com>
2024-12-20 21:46:11 +0100

    feat(fish): add some path and variable stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source "$HOME/.cargo/env.fish"
 end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
