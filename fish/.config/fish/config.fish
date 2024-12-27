b5e2d469eb86e793765f18a16f51cf6c2dc3e847
Lukas Stauersbøl <9026132+stauersbol@users.noreply.github.com>
2024-12-27 23:30:31 +0100

    feat(fish): add bun, zoxide and direnv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,3 +8,14 @@ source "$HOME/.cargo/env.fish"
 # volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# zoxide
+zoxide init fish | source
+
+# direnv
+direnv hook fish | source
+direnv export fish | source
