fd1774e362adb0b4669b40368b1a1429c1c5d163
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-02-05 22:15:47 +0100

    fix(fish): don't source cargo env.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# cargo
-source "$HOME/.cargo/env.fish"
-
 # volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
