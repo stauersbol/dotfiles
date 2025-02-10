13ae5dfab3ed10797400f97c18f378d9ce038780
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-02-10 18:52:52 +0100

    feat(lazyvim): add fish-lsp config

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -43,4 +43,15 @@ return {
       end
     end,
   },
+  {
+    "ndonfris/fish-lsp",
+    config = function()
+      vim.cmd([[
+        autocmd BufWritePre,BufEnter *.fish lua vim.lsp.start({name='fish-lsp', cmd = {'fish-lsp', 'start'}, filetypes = {'fish'}})
+        autocmd CursorHold  <silent><buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorHoldI <silent><buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <silent><buffer> lua vim.lsp.buf.clear_references()
+      ]])
+    end,
+  },
 }
