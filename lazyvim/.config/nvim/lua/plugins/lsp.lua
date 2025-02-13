1b5e872c692e423350891cbbfec2a4bd1c9f609b
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-02-13 21:57:37 +0100

    fix(lazyvim): fish-lsp should just start via nvim-lspconfig

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
         zls = {},
         crystalline = {},
         hyprls = {},
+        fish_lsp = {},
       },
     },
   },
@@ -41,17 +42,6 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "stylelint" })
       end
-    end,
-  },
-  {
-    "ndonfris/fish-lsp",
-    config = function()
-      vim.cmd([[
-        autocmd BufWritePre,BufEnter *.fish lua vim.lsp.start({name='fish-lsp', cmd = {'fish-lsp', 'start'}, filetypes = {'fish'}})
-        autocmd CursorHold  <silent><buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorHoldI <silent><buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <silent><buffer> lua vim.lsp.buf.clear_references()
-      ]])
     end,
   },
 }
