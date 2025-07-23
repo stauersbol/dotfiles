c7760bcd68036c3fa26471941a9a7ad36e219b82
Lukas Stauersbøl <lst@scaletronic.dk>
2025-07-23 10:38:15 +0200

    feat(lazyvim): add autocmd to turn off autoformat for certain projects

--- lazyvim/.config/nvim/lua/config/autocmds.lua
+++ lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable autoformat in selected proejcts due to format rules
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    local disallowed_projects = {
+      ["dws-server"] = true,
+      ["dws-config"] = true,
+    }
+
+    if disallowed_projects[project_name] then
+      vim.g.autoformat = false
+    else
+      vim.g.autoformat = true
+    end
+  end,
+})
