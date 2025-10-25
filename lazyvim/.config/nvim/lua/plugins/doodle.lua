d24ae817707b83703c6e05144b56424366721f6b
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-10-25 19:49:28 +0200

    feat(lazyvim): turn on harpoon and add doodle

---
+++ lazyvim/.config/nvim/lua/plugins/doodle.lua
@@ -0,0 +1,47 @@
+return {
+  "stauersbol/doodle",
+  branch = "add-git-remote-options",
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("doodle").setup({
+      settings = {
+        -- This is the only required setting for sync to work.
+        -- Set it to the absolute path of your private notes repository.
+        git_repo = vim.fn.expand("~/Projects/personal/notes"),
+        sync = true,
+      },
+    })
+  end,
+  keys = {
+    {
+      "<space>a",
+      "",
+      desc = "notes",
+    },
+    {
+      "<space>af",
+      function()
+        require("doodle"):toggle_finder()
+      end,
+      desc = "Doodle Finder",
+    },
+    {
+      "<space>as",
+      function()
+        require("doodle"):sync()
+      end,
+      desc = "Doodle Sync",
+    },
+    {
+      "<space>al",
+      function()
+        require("doodle"):toggle_links()
+      end,
+      desc = "Doodle Links",
+    },
+  },
+}
