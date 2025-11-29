4535e7eafbee6d958de4f64db720df9e0efdd54c
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-11-29 19:55:11 +0100

    feat(ignis): add some initial work on ignis

---
+++ ignis/.config/ignis/modules/bar/bar.py
@@ -0,0 +1,18 @@
+from ignis.widgets import Widget
+
+
+class Bar(Widget.Window):
+    __gtype_name__ = "Bar"
+
+    def __init__(self, monitor: int):
+        super().__init__(
+            anchor=["top"],
+            exclusivity="exclusive",
+            monitor=monitor,
+            namespace=f"ignis_BAR_{monitor}",
+            layer="top",
+            kb_mode="none",
+            child=Widget.CenterBox(
+                css_classes=["bar-widget"],
+            ),
+        )
