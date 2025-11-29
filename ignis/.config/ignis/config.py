220bc9ac7c9ec98ec3a3517b567be0e4271cabfb
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2025-11-29 22:50:03 +0100

    fix(ignis): cleanup main file

--- ignis/.config/ignis/config.py
+++ ignis/.config/ignis/config.py
@@ -1,16 +1,5 @@
 from ignis import utils
 from ignis import widgets
-from ignis.widgets import Widget
-
-
-def left(monitor_name: str) -> Widget.Box:
-    left = Widget.Box(child=[Workspaces(monitor_name)])
-
-
-def bar(monitor_id: int = 0) -> Widget.Window:
-    monitor_name = utils.get_monitor(monitor_id).get_connector()  # type: ignore
-    widgets_box = Widget.CenterBox
-
 
 for i in range(utils.get_n_monitors()):
     bar(i)
