f44bd222888aaefd2e025376e0764c06bb32afda
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2026-06-30 19:04:26 +0200

    feat(fish): add cargo bin to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx PATH "$HOME/.cargo/bin" $PATH
+
 # volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
