5252663b41785860b67a9f026e8da8e2dbf67a76
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2026-09-06 21:22:00 +0200

    fix(hyprland): add missing "/" when loading

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -3,6 +3,6 @@ require("modules.utils")
 
 local host = get_hostname()
 
-if is_file_exists(HOME .. ".config/hypr/modules/hosts" .. host .. ".lua") then
+if is_file_exists(HOME .. "/.config/hypr/modules/hosts/" .. host .. ".lua") then
 	require("modules.hosts." .. host)
 end
